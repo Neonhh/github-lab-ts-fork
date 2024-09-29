5cc16e0194fc9b02a21f8dad8cc33d568896272c
Neonhh <thenjhu@gmail.com>
2024-09-28 20:35:32 -0400

    cambia el require por import

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,12 +1,13 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci : (n : number) => number = require("./fib");
+//const fibonacci : (n : number) => number = require("./fib");
+import fibonacci from './fib';
+import { Request, Response } from 'express';
 
-export default (req, res) => {
-  const { num } : { num: string } = req.params;
-
+export default (req : Request, res : Response) => {
+  const { num } = req.params;
   const fibN : number = fibonacci(parseInt(num));
-  let result : string = `fibonacci(${num}) is ${fibN}`;
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
